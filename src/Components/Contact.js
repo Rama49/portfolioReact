1d4e97b74b7f331c95861d6e2fc48bc67f27da2f
Rama Fall <rama.fall49@gmail.com>
2023-03-09 18:14:21 +0000

    portfolioreact

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -51,16 +51,16 @@ const Contact =() =>{
                 <h2>Get In Touch</h2>
                 <form onSubmit={handleSubmit}>
                     <Row>
-                        <Col sm={6} className='px-1'>
+                        <Col sm={6} className='px-1 mb-5'>
                             <input type='text' value={formDetails.firstName} placeholder='First Name' onChange={(e) => onFormUpdate('firstName', e.target.value)} />
                         </Col>
-                        <Col sm={6} className='px-1'>
+                        <Col sm={6} className='px-1 mb-5'>
                         <input type='text' value={formDetails.firstName} placeholder='Last Name' onChange={(e) => onFormUpdate('LastName', e.target.value)} />
                         </Col>
-                        <Col sm={6} className='px-1'>
+                        <Col sm={6} className='px-1 mb-5'>
                             <input type='email' value={formDetails.email} placeholder='Email Adresse' onChange={(e) => onFormUpdate('email', e.target.value)} />
                         </Col>
-                        <Col sm={6} className='px-1'>
+                        <Col sm={6} className='px-1 mb-5'>
                         <input type='tel' value={formDetails.phone} placeholder='Phone No.' onChange={(e) => onFormUpdate('phone', e.target.value)} />
                         </Col>
                         <Col>
